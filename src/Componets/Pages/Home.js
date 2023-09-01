3421760bf0f18569940a4fa6a5fee559d5e6735a
bhimprasadrajbanshi <rajbanshibhimprasad123@gmail.com>
2023-09-01 14:16:15 +0545

    designe the weather  page

--- src/Componets/Pages/Home.js
+++ src/Componets/Pages/Home.js
@@ -1,12 +1,26 @@
-import React from 'react'
-import './Style.css'
-import Background from '.../public/images/clouds-moving.gif'
+import React from 'react';
+import './Style.css';
+
 
 function Home() {
   return (
    <>
-    <div className="mainbox" style={{backgroundImage:'url(${Background})'}}>
-        <h1>Heloo</h1>
+   <div className='main'>
+    <div className="mainbox" >
+    <div className="title">
+      <h1>WEATHER FORCASTING</h1>
+    </div>
+      <div className="innerbox">
+        <div className="location">
+          <strong> 
+          <p className='locName'>Nepal</p>
+          Setember 25, 2023</strong>
+        </div>
+        <div className="weatherimage">
+          <img src="https://e7.pngegg.com/pngimages/517/940/png-clipart-cloud-rain-sunlight-weather-cloud-text-cloud.png" alt="" />
+        </div>
+        </div>
+    </div>
     </div>
    </>
   )
